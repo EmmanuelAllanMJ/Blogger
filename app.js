365e6816bb050b2218a8abf2f2086cddfeb020a1
Emmanuel Allan <emmanuelallanmj@gmail.com>
2022-07-11 11:11:08 +0530

    Added sign in

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 // const md5 = require("md5");
 
@@ -13,26 +16,118 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+//Authentication
+app.use(
+  session({
+    secret: "Emmanuel Allan M J ",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(bodyParser.urlencoded({ extended: true }));
-//This is used to say node to use static files like styles.css in public folder
-app.use(express.static("public"));
 
 //Connecting to DB
 mongoose.connect("mongodb://localhost:27017/blogDB");
+
+//This is used to say node to use static files like styles.css in public folder
+app.use(express.static("public"));
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  username: String,
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+let username1;
+
+app.get("/signin", function (req, res) {
+  res.render("signIn");
+});
+
+// app.get("/", function (req, res) {
+//   if (req.isAuthenticated()) {
+//     res.render("secrets");
+//   } else {
+//     res.redirect("login");
+//   }
+// });
+
+app.post("/signin", function (req, res) {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        res.redirect("/signin");
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/");
+        });
+      }
+    }
+  );
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.post("/login", function (req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  req.login(user, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        req.session.context = req.body.username; //to save username
+        res.redirect("/");
+      });
+    }
+  });
+});
 
 //Creating Schema
 const blogSchema = {
   name: String,
   content: String,
+  author: String,
 };
 
 //Creating model
 const Post = mongoose.model("Post", blogSchema);
 
 app.get("/", function (req, res) {
+  if (req.session.context != "undefined") {
+    User.find({ email: req.session.context }, function (err, user) {
+      if (err) {
+        console.log(err);
+      } else {
+        username1 = user.username;
+      }
+    });
+  }
   Post.find({}, function (err, posts) {
+    console.log(req.session.context);
     res.render("home", {
       posts: posts,
+      username: username1,
     });
   });
 });
@@ -44,7 +139,11 @@ app.get("/pricing", function (req, res) {
 
 //Creating new blog post
 app.get("/compose", function (req, res) {
-  res.render("compose");
+  if (req.isAuthenticated()) {
+    res.render("compose", { username: req.session.context });
+  } else {
+    res.render("signin");
+  }
 });
 
 //Getting title and content and storing to the DB
@@ -52,6 +151,7 @@ app.post("/compose", function (req, res) {
   const post = new Post({
     name: req.body.postTitle,
     content: req.body.postBody,
+    author: req.session.context.split("@")[0],
   });
   post.save();
   res.render;
@@ -71,8 +171,9 @@ app.get("/posts/:postName", function (req, res) {
         res.render("post", {
           name: post.name,
           content: post.content,
-          author: true,
+          author: post.author,
           url: storedTitle,
+          username: req.session.context,
         });
       }
     });
